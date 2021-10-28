fb12c1dc3942d9c62914dfb2b809c934f3b49b30
Todd Howard <rammgamming@gmail.com>
2021-10-27 23:16:34 -0400

    Fixed start up function

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,14 @@ import { PrismaClient } from '@prisma/client';
 
 const client = new PyneClient();
 
-async () => {
+async function start() {
 	container.db = new PrismaClient();
 
 	client.logger.info('Logging in');
 	await client.start();
 	client.logger.info('logged in');
-};
+}
+
+start().catch((error) => {
+	client.logger.info(error);
+});
