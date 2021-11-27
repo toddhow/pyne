d2630730b34fd33207d77bfb645b8629947a0b1e
Todd Howard <rammgamming@gmail.com>
2021-11-27 11:14:15 -0500

    yarn.lock

--- prisma/migrations/20211030172109_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "GuildSettings" (
-    "id" TEXT NOT NULL,
-    "prefixes" TEXT[],
-    "disabledChannels" TEXT[],
-
-    CONSTRAINT "GuildSettings_pkey" PRIMARY KEY ("id")
-);
--- src/lib/api/methods.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const applicationAdapter = axios.create({
-	baseURL: process.env.API_URL,
-	headers: {
-		Authorization: typeof process.env.TOKEN == undefined ? `Bearer ${process.env.TOKEN}` : ' '
-	}
-});
